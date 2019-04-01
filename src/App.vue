d82bb1824525b150e9473bfbcd5e3ad55e896de2
Andekl <andreas.eklundit94@gmail.com>
2019-04-01 16:04:17 +0200

    started project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <header id="title">Game of Thrones</header>
+  </div>
+</template>
+
+<style>
+#app {
+  background-color: rgb(8, 8, 8);
+  height: 100vh;
+}
+#title {
+  color: rgb(233, 233, 233);
+}
+</style>
